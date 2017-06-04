14de74118669695a1228a08fdc06dd33f6ce4447
jesseharold <jesseharold@gmail.com>
2017-06-03 20:24:17 -0700

    pseudo coded in CRUD routes

--- server.js
+++ server.js
@@ -9,11 +9,14 @@ app.prepare()
 .then(() => {
   const server = express()
 
+  // *** Support for server-side clean URLs routing ***
   server.get('/p/:id', (req, res) => {
     const actualPage = '/post'
     const queryParams = { title: req.params.id } 
     app.render(req, res, actualPage, queryParams)
   })
+
+  // *** Blog Post CRUD Routes ***
 
   server.get('/api/:id', (req, res) => {
       // retrieve a post's info from the db and return it
@@ -26,9 +29,36 @@ app.prepare()
           dateUpdated: "yesterday",
           body: "A very long string with escaped characters, possibly html, not really sure how's best to store this whole post. markdown? just text for now is fine, i guess"
         };
+      //check to make sure this post isn't marked as delted
       res.send(requestedPage);
   })
 
+  server.post('/api/:id', (req, res) => {
+      // save a post to the db and return it
+      console.log('Someone saved post ' + req.params.id);
+      // if id exists in the db, update that document
+      // else, create a new one
+      const newPage = {
+          id: req.params.id,
+          title: "Title of a Brand New Page",
+          author: "Harry the Great",
+          dateCreated: "today",
+          dateUpdated: "just now",
+          body: "A very long string with escaped characters, possibly html, not really sure how's best to store this whole post. markdown? just text for now is fine, i guess"
+        };
+      res.send(newPage);
+  })
+
+  server.delete('/api/:id', (req, res) => {
+      // mark a post as deleted in the db
+      console.log('Someone deleted post ' + req.params.id);
+      // if id exists in the db, update that document
+      res.send("deleted");
+  })
+
+  // *** End of CRUD Routes ***
+
+  
   server.get('*', (req, res) => {
     return handle(req, res)
   })
